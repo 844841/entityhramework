37159741361da1ed3d50756bd1e77999823b55a2
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:04:57 +0530

    Initial commit

---
+++ entityhramework/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace entityhramework
+{
+    class Customer
+    {
+        [Key]
+        public int Cid { get; set; }
+        [Required]
+        public string Cname { get; set; }
+            [MaxLength(250)]
+            public
+    }
+}
---
+++ entityhramework/CustomerContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace entityhramework
+{
+    class CustomerContext:DbContext
+    {
+        public CustomerContext() : base("CustomerContext") { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+        protected override void OnModeCreating(DbModeBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+    }
+}
